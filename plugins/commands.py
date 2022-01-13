d607f9f879eeb80d9d587297ae1407977bd01789
YasuYamamoto <97422484+dipvyash@users.noreply.github.com>
2022-01-13 10:11:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -85,7 +85,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('Movie Channel', url='https://t.me/MovieX_Town')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
